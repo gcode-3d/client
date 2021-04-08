d755bc4c492ce37781311363cbffad609cc7a858
Tobias Feld <tobias.feld@hotmail.com>
2021-04-08 23:21:40 +0200

    Populate homepage with widget that better describes the current state of the printer

---
+++ src/components/stateWidget.jsx
@@ -0,0 +1,233 @@
+import React, { useContext } from "react";
+import ConnectionContext from "./connectionContext";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBug,
+  faExclamationCircle,
+  faPrint,
+  faSpinnerThird,
+} from "@fortawesome/pro-regular-svg-icons";
+import { faCircle } from "@fortawesome/pro-duotone-svg-icons";
+import emitter from "../tools/emitter";
+import "../styles/stateWidget.css";
+import PrintStatusWidget from "./printStatusWidget";
+
+export default function StateWidget() {
+  let context = useContext(ConnectionContext);
+  let printInfo = null;
+  if (
+    context.state == "Printing" &&
+    context.stateDescription != null &&
+    context.stateDescription.printInfo != null
+  ) {
+    console.log(context.stateDescription.printInfo);
+    printInfo = {
+      printTime:
+        context.stateDescription.printInfo.estEndTime !== null
+          ? new Date(context.stateDescription.printInfo.estEndTime).getTime() -
+            new Date(context.stateDescription.printInfo.startTime).getTime()
+          : 0,
+      progress: context.stateDescription.printInfo.progress,
+      tempInfo:
+        context.stateDescription.tempData != null
+          ? context.stateDescription.tempData[
+              context.stateDescription.tempData.length - 1
+            ]
+          : null,
+    };
+  }
+  return (
+    <div className="columns status-widget">
+      <StatusColumn
+        printerName={"harry"}
+        state={context.state}
+        stateDescription={context.stateDescription}
+      />
+      <QuickActions
+        state={context.state}
+        stateDescription={context.stateDescription}
+      />
+      <div className="column is-narrow">
+        {context.state == "Printing" && context.stateDescription.printInfo && (
+          <PrintStatusWidget
+            printTime={printInfo.printTime}
+            progress={printInfo.progress}
+            tempInfo={printInfo.tempInfo}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+function StatusColumn(props) {
+  let color = "#595959";
+  let icon = null;
+  switch (props.state) {
+    case "Disconnected":
+      icon = <FontAwesomeIcon icon={faCircle} />;
+      color = "#595959";
+      break;
+    case "Errored":
+      icon = <FontAwesomeIcon icon={faExclamationCircle} />;
+      color = "#ff0000";
+      break;
+    case "Printing":
+      icon = <FontAwesomeIcon icon={faPrint} />;
+      color = "#2aba2a";
+      break;
+    case "Connected":
+      icon = <FontAwesomeIcon icon={faCircle} />;
+      color = "#2aba2a";
+      break;
+    case "Preparing":
+      icon = <FontAwesomeIcon icon={faSpinnerThird} spin={true} />;
+      color = "##fffc4e";
+      break;
+    case "Connecting":
+      icon = <FontAwesomeIcon icon={faWifi} />;
+      color = "#2fffd8";
+      break;
+    default:
+      icon = <FontAwesomeIcon icon={faBug} />;
+      break;
+  }
+  return (
+    <div className="column">
+      <div className="status-icon" style={{ color }}>
+        {icon}
+      </div>
+      <div className="status-text">
+        <h1 className="title">
+          {props.printerName.includes("printer")
+            ? `${props.printerName[0].toUpperCase()}${props.printerName.slice(
+                1
+              )}`
+            : `${props.printerName[0].toUpperCase()}${props.printerName.slice(
+                1
+              )}-Printer`}
+        </h1>
+        <h4 className="subtitle">
+          {getStatusText(props.state, props.stateDescription)}
+        </h4>
+      </div>
+    </div>
+  );
+
+  function getStatusText(state, description) {
+    switch (state) {
+      case "Errored":
+        if (description.errorDescription != null) {
+          return "Encountered an error: " + description.errorDescription;
+        }
+        return `Encountered an unknown error: Consult the logs for more information.`;
+      case "Disconnected":
+        return "Your printer is disconnected";
+      case "Printing":
+        return `Currently printing ${
+          description.printInfo ? description.printInfo.file.name : "Unknown"
+        }`;
+      case "Connecting":
+        return `Attempting to connect to your printer, this could take a few seconds.`;
+      case "Connected":
+        return `Your printer is connected`;
+      case "Preparing":
+        return `Analysing gcode file for new print ${
+          description.printInfo ? description.printInfo.file.name : "Unknown"
+        }`;
+      default:
+        return `Unknown state: ${state}`;
+    }
+  }
+}
+
+function QuickActions({ state, description }) {
+  let actions = [];
+
+  let connectButton = (
+    <a
+      key="connect"
+      onClick={() => {
+        emitter.emit("client.state.update", "connect");
+      }}
+    >
+      Connect printer
+    </a>
+  );
+
+  let reconnectButton = (
+    <a
+      key="reconnect"
+      onClick={() => {
+        emitter.emit("client.state.update", "reconnect");
+      }}
+    >
+      Reconnect printer
+    </a>
+  );
+
+  let disconnectButton = (
+    <a
+      key="disconnect"
+      onClick={() => {
+        emitter.emit("client.state.update", "disconnect");
+      }}
+    >
+      Disconnect printer
+    </a>
+  );
+
+  let emergencyButton = (
+    <a
+      key="emergency"
+      style={{ color: "#B94C4C" }}
+      onClick={() => {
+        emitter.emit("client.state.update", "EMERGENCY");
+      }}
+    >
+      Emergency shutdown
+    </a>
+  );
+
+  let cancelPrintButton = (
+    <a
+      key="cancel"
+      onClick={() => {
+        emitter.emit("client.state.update", "cancel");
+      }}
+    >
+      cancel printer
+    </a>
+  );
+
+  switch (state) {
+    case "Errored":
+      actions.push(connectButton);
+      break;
+    case "Disconnected":
+      actions.push(connectButton);
+      break;
+    case "Printing":
+    case "Preparing":
+      actions.push(cancelPrintButton);
+      actions.push(reconnectButton);
+      actions.push(disconnectButton);
+      actions.push(emergencyButton);
+      break;
+    case "Connected":
+      actions.push(reconnectButton);
+      actions.push(disconnectButton);
+      actions.push(emergencyButton);
+  }
+  if (actions.length == 0) {
+    return null;
+  }
+
+  return (
+    <div className="column is-narrow has-text-right">
+      <h1 className="title">Quick actions</h1>
+      <div className="spacedList">{actions}</div>
+    </div>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,17 +3,17 @@ import React from "react";
 import TemperatureChart from "../components/temperatureChart";
 
 import PageContainer from "../components/pageContainer";
-import PrintStatusWidget from "../components/printStatus";
+import StateWidget from "../components/stateWidget";
 export default function IndexPage() {
   return (
     <PageContainer page="home">
       <div className="container">
-        <div className="columns">
-          <div className="column is-half" style={{ height: "450px" }}>
-            <TemperatureChart />
+        <div className="columns is-multiline ">
+          <div className="column is-full" style={{ height: "300px" }}>
+            <StateWidget />
           </div>
-          <div className="column is-half">
-            <PrintStatusWidget />
+          <div className="column is-half" style={{ height: "400px" }}>
+            <TemperatureChart />
           </div>
         </div>
       </div>
